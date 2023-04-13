b2a3af1364b1229d0819aa631e23701e13a67e7c
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2023-04-13 16:02:56 +0200

    Add files via upload

---
+++ 1. First Steps in Programming/RectangleArea.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(a * b);
+    }
+}
+
+// Write a Java program that receives two numbers, a and b, then calculates and prints the area of a rectangle with sides a and b.
