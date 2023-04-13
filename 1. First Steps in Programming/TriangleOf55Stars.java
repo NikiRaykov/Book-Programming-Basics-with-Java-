3ba0c85f45e487d7a1c84c82d86449f5f4daad78
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2023-04-13 15:59:19 +0200

    Add files via upload

---
+++ 1. First Steps in Programming/TriangleOf55Stars.java
@@ -0,0 +1,12 @@
+public class TriangleOf55Stars {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Write a Java console program that prints a triangle of 55 stars on 10 lines:
