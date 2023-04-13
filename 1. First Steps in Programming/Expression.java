66af96e7a98f39c24d3416c5a80d416fbd445981
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2023-04-13 15:51:30 +0200

    Add files via upload

---
+++ 1. First Steps in Programming/Expression.java
@@ -0,0 +1,9 @@
+public class Expression {
+    public static void main(String[] args) {
+        System.out.println((3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3);
+    }
+}
+
+// Write a console-based Java program that calculates and prints the value of the following numerical expression:
+//
+//(3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3
