9e033dfd0b978815abdd9666cf6d189f3ff068f9
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2023-04-13 15:53:06 +0200

    Add files via upload

---
+++ 1. First Steps in Programming/Num1To20.java
@@ -0,0 +1,9 @@
+public class Num1To20 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+// Write a Java console program that prints the numbers from 1 to 20 on separate lines on the console.
