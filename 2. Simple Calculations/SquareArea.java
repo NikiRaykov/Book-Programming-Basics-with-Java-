e8e120a0b7f7612f3638834a3ce23bde4d0c6bc9
NikiRaykov <79482102+NikiRaykov@users.noreply.github.com>
2023-04-20 17:30:21 +0200

    Add files via upload

---
+++ 2. Simple Calculations/SquareArea.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SquareArea {
+    public static void main(String[] args) {
+           Scanner scanner = new Scanner(System.in);
+           int a = Integer.parseInt(scanner.nextLine());
+
+            System.out.println(a * a);
+    }
+}
+
+// For example, let's look at the following program which reads an integer from the console,
+// multiplies it by itself (squares it), and prints the result from the multiplication.
